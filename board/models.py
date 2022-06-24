9a54542012194bbd710f19047be1e23758276731
Hyoung.Joon.Kim <hjkim318@naver.com>
2022-06-24 14:05:32 +0900

    Django3 board 모델생성, write.html 유효성 결과 추가

--- board/models.py
+++ board/models.py
@@ -1,13 +1,22 @@
-import datetime
-
+from datetime import datetime
 from django.db import models
+from member.models import Member
 
 # Create your models here.
-class Board(models.Model):
+# on_delete : CASECADE, DO_NOTHING
+
+class board(models.Model):
     id = models.AutoField(primary_key=True)
-    userid = models.CharField(max_length=18)
     title = models.CharField(max_length=100)
-    contxt = models.TextField(max_length=5000)
-    regdate = models.DateTimeField(default=datetime.datetime.now)
-    lpcnt = models.IntegerField(default=0)
+    userid = models.ForeignKey(Member, db_column='userid', on_delete=models.CASCADE)
+    regdate = models.DateTimeField(default=datetime.now)
+    views = models.IntegerField(default=0)
+    contents = models.TextField(null=False, blank=False)
+
+    class Meta:
+        db_table = 'board'
+        ordering = ['-regdate']
+
+    def __str__(self):
+        return self.title
 
